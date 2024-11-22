b63f2eae79e791cf10661714fd0ecd5327e44b78
Jesus Vazquez <97911480+JesusV109@users.noreply.github.com>
2024-11-21 22:30:34 -0500

    Update page.tsx

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -122,7 +122,6 @@ export default function About() {
         <h2 className="text-2xl font-semibold mb-6">Contact</h2>
         <ul className="list-none space-y-2">
           <li>Email: <a href="mailto:albertovazquez86av@gmail.com" className="text-blue-500 hover:underline">albertovazquez86av@gmail.com</a></li>
-          <li>Phone: 732-527-7153</li>
           <li>LinkedIn: <a href="https://www.linkedin.com/in/jesus-vazquez-02b450202" className="text-blue-500 hover:underline">linkedin.com/in/jesus-vazquez-02b450202</a></li>
           <li>GitHub: <a href="https://github.com/JesusV109" className="text-blue-500 hover:underline">github.com/JesusV109</a></li>
         </ul>
